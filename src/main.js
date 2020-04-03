4797080ab3e4871f789804483620c2647281cb4f
岸駿太郎 <shuntaro.kishi@seattleconsulting.co.jp>
2020-04-03 13:43:19 +0900

    ロジック完成

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/ja'
+import {MediaQueries} from 'vue-media-queries'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, {locale});
 
+const mediaQueries = new MediaQueries();
+Vue.use(mediaQueries);
+
 new Vue({
   render: h => h(App),
+  mediaQueries: mediaQueries
 }).$mount('#app')
